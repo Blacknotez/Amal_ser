ca8f061747618a90551aa49e70de452123bf79ef
Blacknotez <94313859+Blacknotez@users.noreply.github.com>
2022-01-22 15:37:51 +0530

    Update contact.js
    BLACKNOTEZ UPDATED

--- plugins/contact.js
+++ plugins/contact.js
@@ -9,9 +9,9 @@ Ktb.addCommand({ pattern: 'git ?(.*)', fromMe: false, desc: 'owner number' }, (a
 //coded by saidali
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:Amalser [OWNER]\n' // full name
-            + 'ORG:Amalser;\n' // 
-            + 'TEL;type=CELL;type=VOICE;waid=919895828468:+91 8289968468\n' // WhatsApp ID + phone number
+            + 'FN:BLACK NOTEZ [OWNER]\n' // full name
+            + 'ORG:ARORA;\n' // 
+            + 'TEL;type=CELL;type=VOICE;waid=91853938626:+91 8289968468\n' // WhatsApp ID + phone number
             + 'END:VCARD'
-await message.client.sendMessage(message.jid,{displayname: "Amalser [OWNER]", vcard: vcard}, MessageType.contact)
+await message.client.sendMessage(message.jid,{displayname: "BLACKNOTEZ [OWNER]", vcard: vcard}, MessageType.contact)
 }))
